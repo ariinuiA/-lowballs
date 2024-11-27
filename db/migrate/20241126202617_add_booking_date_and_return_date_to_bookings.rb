c673c1fbd22597284f0725d0f3f9ced3e1210faf
Jelani Saint Juste <beltchikastjuste.family@gmail.com>
2024-11-27 16:27:20 -0500

    Test push with deleted migrations.

--- db/migrate/20241126202617_add_booking_date_and_return_date_to_bookings.rb
+++
@@ -1,6 +0,0 @@
-class AddBookingDateAndReturnDateToBookings < ActiveRecord::Migration[7.1]
-  def change
-    add_column :bookings, :booking_date, :date
-    add_column :bookings, :return_date, :date
-  end
-end
