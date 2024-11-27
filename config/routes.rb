1880ca66edc7f0c1c029866bea514e606484a123
Anabelle Dubois <anabelle.dubois@icloud.com>
2024-11-27 11:58:37 -0500

    Story to view more details about an itemssubmitting for review

--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  #get 'items/:id' => 'pages#show', as: 'item'
+  resources :items, only: [:index, :show]
 
 end
